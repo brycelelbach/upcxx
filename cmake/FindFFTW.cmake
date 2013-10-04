cffda7ae31a17dee2d41a59f70b19bc32c54959a
Michael Driscoll <mbdriscoll@gmail.com>
2013-10-04 12:13:07 -0700

    pull fftw libraries from cmake find module

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -1,11 +1,14 @@
-# Try to find UPCXX library and include path.
+# Try to find FFTW library and include path.
 # Once done this will define
 #
 # FFTW_FOUND
-# FFTW_CONDUIT_INCLUDE_DIR
 # FFTW_INCLUDE_DIR
+# FFTW_LIBRARY_PATH
 # FFTW_LIBRARIES
 #
+
+# mbd: I wrote this when I didn't have an internet connection,
+# so there may be a production-quality one available online.
 
 include(FindPackageHandleStandardArgs)
 
@@ -30,6 +33,8 @@ find_path(FFTW_LIBRARY_PATH
     DOC
         "The fftw library"
 )
+
+set (FFTW_LIBRARIES fftw3 fftw3_threads)
 
 find_package_handle_standard_args(FFTW DEFAULT_MSG
     FFTW_INCLUDE_DIR
