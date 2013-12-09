8c3a8ab0be0440806e8aac59f6336052d691c09a
Amir Kamil <akamil@gmail.com>
2013-12-08 22:08:49 -0800

    Restructure implementation of reduction operations. Add tentative bulk reduction operations over local, contiguous arrays.

--- examples/cg/Reduce.h
+++ examples/cg/Reduce.h
@@ -2,147 +2,113 @@
 
 #include <collective.h>
 
-template<class T> struct datatype_wrapper {};
-template<> struct datatype_wrapper<char> {
-  typedef char number_type;
-  typedef char int_type;
-  static const upcxx_datatype_t value = UPCXX_CHAR;
-};
-template<> struct datatype_wrapper<unsigned char> {
-  typedef unsigned char number_type;
-  typedef unsigned char int_type;
-  static const upcxx_datatype_t value = UPCXX_UCHAR;
-};
-template<> struct datatype_wrapper<short> {
-  typedef short number_type;
-  typedef short int_type;
-  static const upcxx_datatype_t value = UPCXX_SHORT;
-};
-template<> struct datatype_wrapper<unsigned short> {
-  typedef unsigned short number_type;
-  typedef unsigned short int_type;
-  static const upcxx_datatype_t value = UPCXX_USHORT;
-};
-template<> struct datatype_wrapper<int> {
-  typedef int number_type;
-  typedef int int_type;
-  static const upcxx_datatype_t value = UPCXX_INT;
-};
-template<> struct datatype_wrapper<unsigned int> {
-  typedef unsigned int number_type;
-  typedef unsigned int int_type;
-  static const upcxx_datatype_t value = UPCXX_UINT;
-};
-template<> struct datatype_wrapper<long> {
-  typedef long number_type;
-  typedef long int_type;
-  static const upcxx_datatype_t value = UPCXX_LONG;
-};
-template<> struct datatype_wrapper<unsigned long> {
-  typedef unsigned long number_type;
-  typedef unsigned long int_type;
-  static const upcxx_datatype_t value = UPCXX_ULONG;
-};
-template<> struct datatype_wrapper<long long> {
-  typedef long long number_type;
-  typedef long long int_type;
-  static const upcxx_datatype_t value = UPCXX_LONG_LONG;
-};
-template<> struct datatype_wrapper<unsigned long long> {
-  typedef unsigned long long number_type;
-  typedef unsigned long long int_type;
-  static const upcxx_datatype_t value = UPCXX_ULONG_LONG;
-};
-template<> struct datatype_wrapper<float> {
-  typedef float number_type;
-  typedef float float_type;
-  static const upcxx_datatype_t value = UPCXX_FLOAT;
-};
-template<> struct datatype_wrapper<double> {
-  typedef double number_type;
-  typedef double float_type;
-  static const upcxx_datatype_t value = UPCXX_DOUBLE;
-};
+#define WRAPPER_DECL(T, num_class, type_code)           \
+  template<> struct datatype_wrapper<T> {               \
+    typedef T number_type;                              \
+    typedef T num_class;                                \
+    static const upcxx_datatype_t value = type_code;    \
+  }
 
 #define NUMBER_TYPE(T) typename datatype_wrapper<T>::number_type
 #define INTEGER_TYPE(T) typename datatype_wrapper<T>::int_type
 
-class Reduce {
- private:
-  template<class T> static T reduce(T val, upcxx_op_t op) {
-    T redval = reduce(val, op, 0);
-    T bcval;
-    upcxx::upcxx_bcast(&redval, &bcval, sizeof(T), 0);
-    return bcval;
-  }
-  template<class T> static T reduce(T val, upcxx_op_t op, int dst) {
-    T redval;
-    upcxx::upcxx_reduce(&val, &redval, 1, dst, op, datatype_wrapper<T>::value);
-    return redval;
+#define REDUCE_ALL_DECL(ret_type, op_name, op_code)     \
+  template<class T> static ret_type op_name(T val) {    \
+    return reduce(val, op_code);                        \
   }
 
- public:
-  template<class T> static NUMBER_TYPE(T) add(T val) {
-    return reduce(val, UPCXX_SUM);
-  }
-  template<class T> static NUMBER_TYPE(T) mult(T val) {
-    return reduce(val, UPCXX_PROD);
-  }
-  template<class T> static NUMBER_TYPE(T) max(T val) {
-    return reduce(val, UPCXX_MAX);
-  }
-  template<class T> static NUMBER_TYPE(T) min(T val) {
-    return reduce(val, UPCXX_MIN);
+#define REDUCE_TO_DECL(ret_type, op_name, op_code)              \
+  template<class T> static ret_type op_name(T val, int root) {  \
+    return reduce(val, op_code, root);                          \
   }
 
-  template<class T> static INTEGER_TYPE(T) lor(T val) {
-    return reduce(val, UPCXX_LOR);
-  }
-  template<class T> static INTEGER_TYPE(T) lxor(T val) {
-    return reduce(val, UPCXX_LXOR);
-  }
-  template<class T> static INTEGER_TYPE(T) land(T val) {
-    return reduce(val, UPCXX_LAND);
-  }
-  template<class T> static INTEGER_TYPE(T) bor(T val) {
-    return reduce(val, UPCXX_BOR);
-  }
-  template<class T> static INTEGER_TYPE(T) bxor(T val) {
-    return reduce(val, UPCXX_BXOR);
-  }
-  template<class T> static INTEGER_TYPE(T) band(T val) {
-    return reduce(val, UPCXX_BAND);
+#define REDUCE_ARRAY_ALL_DECL(val_type, op_name, op_code)                  \
+  template<class T, int N> static void op_name(ndarray<val_type, N> src,   \
+                                               ndarray<val_type, N> dst) { \
+    return reduce(src, dst, op_code);                                      \
   }
 
-  template<class T> static NUMBER_TYPE(T) add(T val, int dst) {
-    return reduce(val, UPCXX_SUM, dst);
-  }
-  template<class T> static NUMBER_TYPE(T) mult(T val, int dst) {
-    return reduce(val, UPCXX_PROD, dst);
-  }
-  template<class T> static NUMBER_TYPE(T) max(T val, int dst) {
-    return reduce(val, UPCXX_MAX, dst);
-  }
-  template<class T> static NUMBER_TYPE(T) min(T val, int dst) {
-    return reduce(val, UPCXX_MIN, dst);
+#define REDUCE_ARRAY_TO_DECL(val_type, op_name, op_code)                 \
+  template<class T, int N> static void op_name(ndarray<val_type, N> src, \
+                                               ndarray<val_type, N> dst, \
+                                               int root) {               \
+    return reduce(src, dst, op_code, root);                              \
   }
 
-  template<class T> static INTEGER_TYPE(T) lor(T val, int dst) {
-    return reduce(val, UPCXX_LOR, dst);
-  }
-  template<class T> static INTEGER_TYPE(T) lxor(T val, int dst) {
-    return reduce(val, UPCXX_LXOR, dst);
-  }
-  template<class T> static INTEGER_TYPE(T) land(T val, int dst) {
-    return reduce(val, UPCXX_LAND, dst);
-  }
-  template<class T> static INTEGER_TYPE(T) bor(T val, int dst) {
-    return reduce(val, UPCXX_BOR, dst);
-  }
-  template<class T> static INTEGER_TYPE(T) bxor(T val, int dst) {
-    return reduce(val, UPCXX_BXOR, dst);
-  }
-  template<class T> static INTEGER_TYPE(T) band(T val, int dst) {
-    return reduce(val, UPCXX_BAND, dst);
-  }
-};
+#define REDUCE_DECLS(val_type, op_name, op_code)        \
+  REDUCE_ALL_DECL(val_type, op_name, op_code)           \
+  REDUCE_TO_DECL(val_type, op_name, op_code)            \
+  REDUCE_ARRAY_ALL_DECL(val_type, op_name, op_code)     \
+  REDUCE_ARRAY_TO_DECL(val_type, op_name, op_code)
+
+namespace upcxx {
+
+  template<class T> struct datatype_wrapper {};
+  WRAPPER_DECL(char, int_type, UPCXX_CHAR);
+  WRAPPER_DECL(unsigned char, int_type, UPCXX_UCHAR);
+  WRAPPER_DECL(short, int_type, UPCXX_SHORT);
+  WRAPPER_DECL(unsigned short, int_type, UPCXX_USHORT);
+  WRAPPER_DECL(int, int_type, UPCXX_INT);
+  WRAPPER_DECL(unsigned int, int_type, UPCXX_UINT);
+  WRAPPER_DECL(long, int_type, UPCXX_LONG);
+  WRAPPER_DECL(unsigned long, int_type, UPCXX_ULONG);
+  WRAPPER_DECL(long long, int_type, UPCXX_LONG_LONG);
+  WRAPPER_DECL(unsigned long long, int_type, UPCXX_ULONG_LONG);
+  WRAPPER_DECL(float, float_type, UPCXX_FLOAT);
+  WRAPPER_DECL(double, float_type, UPCXX_DOUBLE);
+
+  class Reduce {
+  private:
+    template<class T> static T reduce(T val, upcxx_op_t op) {
+      T redval = reduce(val, op, 0);
+      T bcval;
+      upcxx_bcast(&redval, &bcval, sizeof(T), 0);
+      return bcval;
+    }
+
+    template<class T> static T reduce(T val, upcxx_op_t op, int root) {
+      T redval;
+      upcxx_reduce(&val, &redval, 1, root, op, datatype_wrapper<T>::value);
+      return redval;
+    }
+
+    template<class T, int N> static void reduce(ndarray<T, N> src,
+                                                ndarray<T, N> dst,
+                                                upcxx_op_t op) {
+      reduce(src, dst, op, 0);
+      upcxx_bcast(dst.storage_ptr(), dst.storage_ptr(),
+                  dst.size() * sizeof(T), 0);
+    }
+
+    template<class T, int N> static void reduce(ndarray<T, N> src,
+                                                ndarray<T, N> dst,
+                                                upcxx_op_t op,
+                                                int root) {
+      upcxx_reduce(src.storage_ptr(), dst.storage_ptr(), src.size(),
+                   root, op, datatype_wrapper<T>::value);
+    }
+
+  public:
+    REDUCE_DECLS(NUMBER_TYPE(T), add,  UPCXX_SUM)
+    REDUCE_DECLS(NUMBER_TYPE(T), mult, UPCXX_PROD)
+    REDUCE_DECLS(NUMBER_TYPE(T), max,  UPCXX_MAX)
+    REDUCE_DECLS(NUMBER_TYPE(T), min,  UPCXX_MIN)
+
+    REDUCE_DECLS(INTEGER_TYPE(T), lor,  UPCXX_LOR)
+    REDUCE_DECLS(INTEGER_TYPE(T), lxor, UPCXX_LXOR)
+    REDUCE_DECLS(INTEGER_TYPE(T), land, UPCXX_LAND)
+    REDUCE_DECLS(INTEGER_TYPE(T), bor,  UPCXX_BOR)
+    REDUCE_DECLS(INTEGER_TYPE(T), bxor, UPCXX_BXOR)
+    REDUCE_DECLS(INTEGER_TYPE(T), band, UPCXX_BAND)
+  };
+
+} /* namespace upcxx */
+
+#undef WRAPPER_DECL
+#undef NUMBER_TYPE
+#undef INTEGER_TYPE
+#undef REDUCE_ALL_DECL
+#undef REDUCE_TO_DECL
+#undef REDUCE_ARRAY_ALL_DECL
+#undef REDUCE_ARRAY_TO_DECL
+#undef REDUCE_DECLS
