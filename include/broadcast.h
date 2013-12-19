0511a85f8328890839fa982ae33dd93661b6313a
Amir Kamil <akamil@lbl.gov>
2013-12-18 22:43:28 -0800

    Safe broadcast doc update.

--- include/broadcast.h
+++ include/broadcast.h
@@ -1,3 +1,16 @@
+/**
+ * broadcast.h - implements a safe broadcast scheme that converts local
+ * pointers and Titanium-style arrays to global pointers and arrays
+ * when broadcasting. Any type can be added to this scheme by defining
+ * a global_type typedef within the type and an explicit conversion to
+ * that global_type. Also implements bulk broadcasts over any array
+ * that defines a local_elem_type typedef and storage_ptr() and size()
+ * methods.
+ * \see collective.h for low-level broadcast interface
+ */
+
+#pragma once
+
 #include <global_ptr.h>
 #include <collective.h>
 
