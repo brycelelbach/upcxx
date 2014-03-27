323714c2f70afeab03317a90c0e8904c494ccba5
Amir Kamil <akamil@lbl.gov>
2014-03-26 21:57:35 -0700

    Add upcxx qualifier in array defs.

--- include/array_defs.h
+++ include/array_defs.h
@@ -43,25 +43,26 @@
 
 #define UPCXXA_LOCAL_TO_GLOBAL_COPY(local, global, nitems)      \
   UPCXXA_ASSIGN_GLOBAL_ANONYMOUS_BULK(global, nitems, local)
-#define UPCXXA_GPTR_TO_T global_ptr<T>
-#define UPCXXA_GREF_TO_T global_ref<T>
+#define UPCXXA_GPTR_TO_T upcxx::global_ptr<T>
+#define UPCXXA_GREF_TO_T upcxx::global_ref<T>
 #define UPCXXA_GET_BOXID(gptr) (UPCXXA_TO_BOX(gptr)).id()
 #define UPCXXA_BOX_T node
 #define UPCXXA_TO_BOX(ptr) (ptr).where()
 #define UPCXXA_TO_LOCAL(ptr) (ptr).raw_ptr()
 #define UPCXXA_TO_GLOBALB(global, box, local)   \
-  (global) = global_ptr<T>(local, box)
+  (global) = upcxx::global_ptr<T>(local, box)
 #define UPCXXA_EQUAL_GLOBAL(p, q) ((p) == (q))
-#define UPCXXA_MYBOX my_node
+#define UPCXXA_MYBOX upcxx::my_node
 #define UPCXXA_IS_DIRECTLY_ADDRESSABLE(ptr)     \
-  ((ptr.where().id()) == my_node.id())
+  ((ptr.where().id()) == upcxx::my_node.id())
 #define UPCXXA_GLOBALIZE(global, local)                 \
   UPCXXA_TO_GLOBALB(global, UPCXXA_MYBOX, local)
 #define UPCXXA_PROC_TO_BOXID_PROC(proc, boxid, bproc)           \
-  global_machine.cpu_id_global2local(proc, boxid, bproc) 
-#define UPCXXA_BOXID_TO_BOX(boxid) global_machine.get_node(boxid)
-#define UPCXXA_BOX_TO_FIRST_PROC(box)           \
-  global_machine.cpu_id_local2global(box, 0)
+  upcxx::global_machine.cpu_id_global2local(proc, boxid, bproc)
+#define UPCXXA_BOXID_TO_BOX(boxid)              \
+  upcxx::global_machine.get_node(boxid)
+#define UPCXXA_BOX_TO_FIRST_PROC(box)                   \
+  upcxx::global_machine.cpu_id_local2global(box, 0)
 
 #define UPCXXA_PUT_NB_BULK(phandle, destbox, destaddr, srcaddr, nbytes) \
   *phandle = gasnet_put_nb_bulk(destbox, destaddr, srcaddr, nbytes)
@@ -84,9 +85,9 @@
   do {                                                          \
     gasnet_coll_broadcast(current_gasnet_team(), dst, sender,   \
 			  src, size, UPCXX_GASNET_COLL_FLAG);   \
-  } while (0)               
+  } while (0)
 #define UPCXXA_EXCHANGE_BULK(dst, src, bytes)                   \
   do {                                                          \
     gasnet_coll_gather_all(current_gasnet_team(), dst, src,     \
                            bytes, UPCXX_GASNET_COLL_FLAG);      \
-  } while (0)               
+  } while (0)
