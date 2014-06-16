16785663061dcf1ce17a2cd9edb3600e85052d04
yzheng <yzheng@lbl.gov>
2014-06-15 21:29:14 -0700

    Add an example about copying and executing a lambda function with closure on a remote node

---
+++ examples/basic/test_copy_closure.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <upcxx.h>
+#include <finish.h>
+
+#ifndef UPCXX_HAVE_CXX11
+int main(int argc, char **argv)
+{
+  upcxx::init(&argc, &argv);
+  printf("This test is for C++11 only.  Please enable C++11 with UPC++ by --enable-cxx11 at configure time.\n");
+  upcxx::finalize();
+  return 0;
+}
+#else // UPCXX_HAVE_CXX11
+
+template <typename T>
+void lambda_wrapper(upcxx::global_ptr<T> remote_lambda)
+{
+  upcxx::global_ptr<T> my_lambda = upcxx::allocate<T>(upcxx::myrank(), 1);
+  upcxx::copy(remote_lambda, my_lambda, 1);
+  (*(T*)my_lambda)(); // execute the lambda
+  upcxx::deallocate(my_lambda);
+  upcxx::deallocate(remote_lambda);
+}
+
+int main(int argc, char **argv)
+{
+  upcxx::init(&argc, &argv);
+  int i, j;
+
+  finish {
+    for (i=0; i<3; i++) {
+      for (j=0; j<2; j++) {
+        upcxx::rank_t src_rank = upcxx::myrank();
+        auto lambda = [=] () {
+          printf("lambda task from src rank %u, i %d, j %d, executing on rank %u\n",
+                 src_rank, i, j, upcxx::myrank());
+        };
+        upcxx::global_ptr<decltype(lambda)> remote_lambda 
+          = upcxx::allocate<decltype(lambda)>(src_rank, 1);
+        memcpy(remote_lambda.raw_ptr(), &lambda, sizeof(decltype(lambda)));
+        upcxx::async((src_rank+1)%upcxx::ranks())(lambda_wrapper<decltype(lambda)>, remote_lambda);
+      }
+    }
+  } // close finish
+
+  upcxx::finalize();
+  return 0;
+}
+#endif // close #ifndef UPCXX_HAVE_CXX11
