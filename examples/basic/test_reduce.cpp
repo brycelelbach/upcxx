b2b47576937d810c7a3590659a7b7883b8889ed0
yzheng <yzheng@lbl.gov>
2014-06-30 23:24:17 -0700

    Remove imcomplete edits in test_reduce.cpp

--- examples/basic/test_reduce.cpp
+++ examples/basic/test_reduce.cpp
@@ -56,56 +56,6 @@ void test(upcxx_datatype_t dt, size_t count)
   barrier();
 }
 
-template<class T>
-int test_team(upcxx_datatype_t dt, size_t count)
-{
-  global_ptr<T> src;
-  global_ptr<T> dst;
-  
-  src = allocate<T>(myrank(), count);
-  dst = allocate<T>(myrank(), count);
-  
-#ifdef DEBUG
-  cerr << myrank() << " scr: " << src << "\n";
-  cerr << myrank() << " dst: " << dst << "\n";
-#endif
-  
-  team_split()
-
-  // Initialize data pointed by host_ptr by a local pointer
-  T *lsrc = (T *)src;
-  for (int i=0; i<count; i++) {
-    lsrc[i] = (T)i + (T)MYTHREAD / THREADS;
-  }
-  
-  barrier();
-  
-  uint32_t root = 0;
-  upcxx_reduce<T>(src, dst, count, root, UPCXX_MAX, dt);
-  
-  if (MYTHREAD == 0) {
-    cout << myrank() << " dst: ";
-    T *ldst = (T *)dst;
-    for (int i=0; i<count; i++) {
-      cout << ldst[i] << " ";
-    }
-    cout << endl;
-  }
-  
-  upcxx_reduce<T>(src, dst, count, root, UPCXX_SUM, dt);
-  
-  if (MYTHREAD == 0) {
-    cout << myrank() << " dst: ";
-    T *ldst = (T *)dst;
-    for (int i=0; i<count; i++) {
-      cout << ldst[i] << " ";
-    }
-    cout << endl;
-  }
-
-  barrier();
-}
-
 int main(int argc, char **argv)
 {
   // \Todo: group all init functions together in a global init function
