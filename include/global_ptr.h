brycelelbach/upcxx

commit 2ba6c685647c1e66023a7fd5f4f83d2cca8da187

Support PSHM in array library.

include/global_ptr.h
@@ -145,6 +145,23 @@ namespace upcxx
 #endif
     }
 
+    T *localize() const
+    {
+      return this->operator T*();
+    }
+
+    bool is_local() const
+    {
+      if (this->where() == global_myrank()) {
+        return true;
+      }
+#if GASNET_PSHM
+      return is_memory_shared_with(this->where());
+#else
+      return false;
+#endif
+    }
+
     template <typename T2>
     global_ref<T> operator [] (T2 i) const
     {

include/array_defs.h
@@ -49,13 +49,14 @@
 #define UPCXXA_GET_BOXID(gptr) UPCXXA_TO_BOX(gptr)
 #define UPCXXA_BOX_T rank_t
 #define UPCXXA_TO_BOX(ptr) (ptr).where()
-#define UPCXXA_TO_LOCAL(ptr) (ptr).raw_ptr()
+#define UPCXXA_TO_LOCAL(ptr) (ptr).localize()
 #define UPCXXA_TO_GLOBALB(global, box, local)   \
   (global) = upcxx::global_ptr<T>(local, box)
+// UPCXXA_EQUAL_GLOBAL needs to be fixed for PSHM
 #define UPCXXA_EQUAL_GLOBAL(p, q) ((p) == (q))
 #define UPCXXA_MYBOX upcxx::myrank()
 #define UPCXXA_IS_DIRECTLY_ADDRESSABLE(ptr)     \
-  ((ptr.where()) == upcxx::myrank())
+  (ptr).is_local()
 #define UPCXXA_GLOBALIZE(global, local)                 \
   UPCXXA_TO_GLOBALB(global, UPCXXA_MYBOX, local)
 #define UPCXXA_PROC_TO_BOXID_PROC(proc, boxid, bproc) do {      \

commit 48a92903e30be88c35aa24335aa6d0abfa1757f2

Fix an issue of getting the local pointer in PSHM for arrays (introduced in commit 2ba6c68)

include/global_ptr.h
@@ -147,7 +147,10 @@ namespace upcxx
 
     T *localize() const
     {
-      return this->operator T*();
+      if (is_local())
+        return this->operator T*();
+      else
+        return this->raw_ptr();
     }
 
     bool is_local() const